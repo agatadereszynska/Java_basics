8e1c452f73bc0009f53ce61b42a07c853a84b0cc
youagata <youagata@mailinator.com>
2022-04-20 20:41:16 +0200

    training

---
+++ src/Switch.java
@@ -0,0 +1,27 @@
+public class Switch {
+    public static void main(String[] args) {
+       int a = 11;
+        switch (a){
+            case 1:
+                System.out.println("poniedzialek");
+                break;
+            case 2:
+                System.out.println("wtorek");
+                break;
+            case 3:
+                System.out.println("sroda");
+                break;
+            case 4:
+                System.out.println("czwartek");
+                break;
+            case 5:
+                System.out.println("piatek");
+                break;
+            case 6:
+                System.out.println("sobota");
+
+            default:
+                System.out.println("papa");
+        }
+    }
+}
